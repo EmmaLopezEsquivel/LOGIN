03912eef5aa8dd17a51c812795059c8ae80b197b
EmmaLopezEsquivel <123218003+EmmaLopezEsquivel@users.noreply.github.com>
2023-01-20 22:53:13 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace LoginUnisur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AVISOS = new System.Windows.Forms.Label();
+            this.txbxPassword = new System.Windows.Forms.TextBox();
+            this.txbxUsuario = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 231);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Verdana", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(500, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UNISUR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(397, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "USUARIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(397, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PASSWORD";
+            // 
+            // AVISOS
+            // 
+            this.AVISOS.AutoSize = true;
+            this.AVISOS.Location = new System.Drawing.Point(163, 352);
+            this.AVISOS.Name = "AVISOS";
+            this.AVISOS.Size = new System.Drawing.Size(23, 13);
+            this.AVISOS.TabIndex = 4;
+            this.AVISOS.Text = "****";
+            // 
+            // txbxPassword
+            // 
+            this.txbxPassword.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txbxPassword.Location = new System.Drawing.Point(503, 190);
+            this.txbxPassword.Name = "txbxPassword";
+            this.txbxPassword.Size = new System.Drawing.Size(142, 20);
+            this.txbxPassword.TabIndex = 5;
+            // 
+            // txbxUsuario
+            // 
+            this.txbxUsuario.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txbxUsuario.Location = new System.Drawing.Point(503, 138);
+            this.txbxUsuario.Name = "txbxUsuario";
+            this.txbxUsuario.Size = new System.Drawing.Size(142, 20);
+            this.txbxUsuario.TabIndex = 6;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(439, 312);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 7;
+            this.btnEntrar.Text = "ENTRAR";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(600, 312);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txbxUsuario);
+            this.Controls.Add(this.txbxPassword);
+            this.Controls.Add(this.AVISOS);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Login Unisur";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label AVISOS;
+        private System.Windows.Forms.TextBox txbxPassword;
+        private System.Windows.Forms.TextBox txbxUsuario;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginUnisur
+{
+    public partial class Form1 : Form
+    {
+        static int intentos = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            // usuario = "emmanuel" password= emma10
+            string usuario = txbxUsuario.Text;
+            string password = txbxPassword.Text;
+
+            if(this.txbxUsuario.Text == "emmanuel" && this.txbxPassword.Text == "emma10")
+            {
+                intentos = 0;
+
+                // picture.Image = new Bitmap(@"C:\Users\NEMEZIZ\source\repos\LoginUnisur\desbloqueospiderman.jpg");
+
+                MessageBox.Show("BIENVENIDO");
+
+ 
+            }
+            else if (intentos <= 3 && intentos > 0)
+            {
+                AVISOS.Text = "solo tienes "+Convert.ToString(intentos)+ "  intentos mas";
+                intentos--;
+            }
+            else
+            {
+                MessageBox.Show("usuario o contraseña incorrecta");
+                // intentos++;
+                Application.Exit();
+            }
+        }
+    }
+}
